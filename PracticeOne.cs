0e4dbb5c6e9f33ed153614187b8c63663b84b67c
Jaden Ciminieri <jadenciminieri@gmail.com>
2022-05-25 10:57:28 -0600

    Practice One

---
+++ PracticeOne.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+            // Declare the Vars
+            int myNum = 50; // Exercise 1 Vars
+            string myName = "Jaden"; // Exercise 2 Vars
+            int x = 5; // Exercise 3 & 4 Vars
+            int y = 10;
+            int z = x + y; // Exercise 4 Vars
+            int x2 = 5, y2 = 6, z2 = 50; // Exercise 5 Vars
+            int xyz = x2 + y2 + z2;
+            
+
+            // Call to console
+            Console.WriteLine("Your lucky number is " + myNum); // Exercise 1
+            Console.WriteLine("Your name is " + myName); // Exercise 2
+            Console.WriteLine("Exercise 3: " + (x + y)); // Exercise 3
+            Console.WriteLine("Exercise 4: " + z); // Excercise 4
+            Console.WriteLine("Exercise 5: " + xyz); // Exercise 5
+
+    }
+}
