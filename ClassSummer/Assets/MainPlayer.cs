0bed8a2143beaf5a397df7e9a159fcda601d9b7f
Jaden Ciminieri <jadenciminieri@gmail.com>
2022-06-23 17:09:19 -0600

    1st Part of Unity Assignments

---
+++ ClassSummer/Assets/MainPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+    public string myName = "Jaden";
+
+    // Use this for initalization
+    void Start()
+    {
+        Debug.Log("I am alive and my name is" + myName + "!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
