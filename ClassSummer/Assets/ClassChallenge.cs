925f255467a45f265b8aefa18b6586c669aa3cef
Jaden Ciminieri <jadenciminieri@gmail.com>
2022-06-25 15:56:09 -0600

    Class Challenge

---
+++ ClassSummer/Assets/ClassChallenge.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassChallenge : MonoBehaviour
+{
+
+    void Start()
+    {
+        int oneMath = 4;
+        int secondMath = 3;
+        int thirdMath = 0;
+
+        oneMath + secondMath = thirdMath;
+        if (thirdMath == 7)
+        {
+            Debug.Log("It equals 7.");
+        }
+    }
+
+}
