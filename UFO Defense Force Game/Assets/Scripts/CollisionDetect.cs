7be8f6b720e8f6b21649bbb4497162e974591c49
Jaden Ciminieri <jadenciminieri@gmail.com>
2022-07-21 22:59:11 -0600

    6c.2 Lesson Finished!

--- UFO Defense Force Game/Assets/Scripts/CollisionDetect.cs
+++ UFO Defense Force Game/Assets/Scripts/CollisionDetect.cs
@@ -6,6 +6,7 @@ public class CollisionDetect : MonoBehaviour
 {
 
     public GameObject gameController;
+    public int scoreAmount;
 
 
     public void Awake()
@@ -18,20 +19,24 @@ public class CollisionDetect : MonoBehaviour
 
        if(other.tag != "Player" && other.tag != "PowerUp" && other.tag != "Enemy")
        {
-           Destroy(other.gameObject);  
-       }
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            gameController.GetComponent<GameController>().AdjustScore(scoreAmount);
+        }
 
         if(other.tag == "Player")
         {
             if (other.gameObject.GetComponent<PlayerController>().playerInDamage == false)
             {
+                Destroy(gameObject);
                 gameController.GetComponent<GameController>().AdjustHealth(-1);
                 other.gameObject.GetComponent<PlayerController>().playerInDamage = true;
             } 
 
         }
 
-        Destroy(gameObject);
+        
+
 
     }
 }
--- UFO Defense Force Game/Assets/Scripts/GameController.cs
+++ UFO Defense Force Game/Assets/Scripts/GameController.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
     private static int health = 3;
-    private static int maxHealth = 5;
+    private static int maxHealth = 3;
     private static int score = 0;
 
     public int Health = health;
     public int MaxHealth = maxHealth;
     public int Score = score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI healthText;
+
+    private void Awake()
+    {
+        scoreText.text = "Score: " + score;
+        healthText.text = "Health: " + health + "/" + maxHealth;
+    }
 
     public void Update()
     {
@@ -27,21 +36,36 @@ public class GameController : MonoBehaviour
         {
             Time.timeScale = 1;
         }
+
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
     }
 
-    public void AdjustHealth(int amount)
+    public void AdjustHealth(int amount) // Adjust the health
     {
         health += amount;
+        UpdateText();
     }
 
-    public void AdjustScore (int amount)
+    public void AdjustScore (int amount) // Adjust the Score
     {
         score += amount;
+        UpdateText();
     }
 
-    public void AdjustMaxHealth(int amount)
+    public void AdjustMaxHealth(int amount) // Adjusts the max health
     {
         maxHealth += amount;
+        UpdateText();
     }
 
+
+    public void UpdateText() // Update all text
+    {
+        scoreText.text = "Score: " + score;
+        healthText.text = "Health: " + health + "/"+ maxHealth;
+    }
 }
